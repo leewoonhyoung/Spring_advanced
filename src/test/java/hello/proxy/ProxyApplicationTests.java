c59c19e2b6a4c3287e2e120ea9790776d485ca82
leewoonhyoung <wh3619@gmail.com>
2022-03-18 20:44:18 +0900

    2022-03-18 GOF pattern (proxy pattern + decorated pattern)

---
+++ src/test/java/hello/proxy/ProxyApplicationTests.java
@@ -0,0 +1,15 @@
+package hello.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
+
+
