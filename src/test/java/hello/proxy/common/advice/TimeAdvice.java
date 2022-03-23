f323a04484c2fdd128734fbbc5aa2db110d769fb
leewoonhyoung <wh3619@gmail.com>
2022-03-23 22:25:41 +0900

    2022-03-23 GOF pattern (Spring ProxyFactory 기술)

---
+++ src/test/java/hello/proxy/common/advice/TimeAdvice.java
@@ -0,0 +1,27 @@
+package hello.proxy.common.advice;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aopalliance.intercept.MethodInterceptor;
+import org.aopalliance.intercept.MethodInvocation;
+
+@Slf4j
+public class TimeAdvice implements MethodInterceptor {
+
+    @Override
+    public Object invoke(MethodInvocation invocation) throws Throwable {
+        log.info("TimeProxy 실행");
+        long startTime = System.currentTimeMillis();
+        Object result = invocation.proceed();
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("TimeProxy 종료 resultTime = {}ms", resultTime);
+        return result;
+        /**
+         * 과거 코드
+         * Object result= method.invoke(target, args);
+         *
+         * */
+
+
+    }
+}
